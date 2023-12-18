dd1d3d6e4aeebe57b0b4fb6528e1ed3355033c23
Alex <alex.chenqr@gmail.com>
2023-12-18 15:24:45 +0800

    update

--- contracts/RouletteGame.sol
+++ contracts/RouletteGame.sol
@@ -6,8 +6,16 @@ import "./JettonPool.sol";
 
 contract RouletteGame is Roulette, JettonPool {
 
-    function makeBet(address player, uint256 amount) public override {
+    function makeBet(address player, uint256 betAmount, uint8 betNumber) public override {
+        // player is not address 0
+        require(player != address(0), "player is not address 0");
+        // betAmount is zero
+        require(betAmount > 0, "betAmount is zero");
+        // betNumber is betwenn 0 and 36
+        require(betNumber >= 0 && betNumber <= 36, "betNumber is betwenn 0 and 36");
 
+        BetInfo memory info = BetInfo(player, betAmount, betNumber);
+        players[player] = info;
     }
 
     function play(address player) public override {
