a6a0c6cdcbfc3db0c731d743b93e39b4fff9bd2a
Alex <alex.chenqr@gmail.com>
2023-12-19 15:05:15 +0800

    update

--- test/TestRouletteGame.js
+++ test/TestRouletteGame.js
@@ -1,12 +1,13 @@
 const { expect } = require("chai");
 
 describe("Test RouletteGame", function() {
-    it("奖金池的余额必须大于0", async function() {
+    const prizePoolBalance = 100000;
+    it(`奖金池的余额必须等于${prizePoolBalance}`, async function() {
         const [owner] = await ethers.getSigners();
 
         const RouletteGame = await ethers.getContractFactory("RouletteGame");
 
         const rouletteGame = await RouletteGame.deploy();
-        expect(await rouletteGame.prizePoolBalance()).to.not.equal(0);
+        expect(await rouletteGame.prizePoolBalance()).to.equal(prizePoolBalance);
     });
 });
