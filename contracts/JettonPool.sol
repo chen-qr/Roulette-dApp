3ae2d998ac533800a9f5141d0117f4bf1856f1b9
Alex <alex.chenqr@gmail.com>
2023-12-21 12:25:17 +0800

    update

--- contracts/JettonPool.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.20;
-
-abstract contract JettonPool {
-
-    address public owner;
-    uint256 public prizePoolBalance;
-    mapping(address => uint256) public playersBlance;
-}
