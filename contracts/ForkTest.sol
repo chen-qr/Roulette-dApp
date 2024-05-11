59db062f9174b91c249fa982905dec61d83edea8
Alex <alex.chenqr@gmail.com>
2024-05-11 11:54:26 +0800

    add

--- contracts/ForkTest.sol
+++ contracts/ForkTest.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
-contract HardhatFork {
+contract ForkTest {
 
     function getBalance(address v) external view returns(uint256) {
         return address(v).balance;
